255bf23e7bb82302e1f765c7432c6ec4a4c3a5fc
rasna shakya <rasnashakya@gmail.com>
2017-02-27 05:23:53 +0545

    added validation for video, added show button on index action

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,8 @@ class Post < ApplicationRecord
 	mount_uploader :video, VideoUploader
   
   ##validation
-  # validates :title, presence: true, length: { minimum: 5}
+  validates :title, presence: true, length: { minimum: 5}
   validates :description, presence: true, length: (10..50)
+  # validates :video, presence: true
 
 end
