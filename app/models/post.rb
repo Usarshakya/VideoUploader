b1fa5e30ceb512a2b05471e64c3b1c9cf8f1e28a
rasna shakya <rasnashakya@gmail.com>
2017-02-27 00:16:32 +0545

    made correction

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,6 @@ class Post < ApplicationRecord
 	mount_uploader :video, VideoUploader
   
   ##validation
-  validates :description, presence: true,
-                          length: [10..20]
+  validates :description, presence: true, length: (10..20)
 
 end
