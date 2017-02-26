727822d0e3a650637918d27f85ebfc017fdd6434
rasna shakya <rasnashakya@gmail.com>
2017-02-27 04:36:37 +0545

    added column title topost and added field for title in _form.html.erb

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170226200155) do
+ActiveRecord::Schema.define(version: 20170226224818) do
 
   create_table "posts", force: :cascade do |t|
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.string   "video"
+    t.text     "title"
   end
 
   create_table "users", force: :cascade do |t|
