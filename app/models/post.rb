124dc95115ea8e15c5c6b852832db02f824f9e16
rasna shakya <rasnashakya@gmail.com>
2017-02-27 04:58:01 +0545

    added title aspost params to ost controller

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,7 +3,7 @@ class Post < ApplicationRecord
 	mount_uploader :video, VideoUploader
   
   ##validation
-  validates :title, presence: true, length: (10..20)
+  # validates :title, presence: true, length: { minimum: 5}
   validates :description, presence: true, length: (10..50)
 
 end
