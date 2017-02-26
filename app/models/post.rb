ca6ea9aa89e41e5b6c069c86916bfdb331ebde37
rasna shakya <rasnashakya@gmail.com>
2017-02-27 00:08:31 +0545

    added validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ApplicationRecord
+
 	mount_uploader :video, VideoUploader
+  
+  ##validation
+  validates :description, presence: true,
+                          length: [10..20]
+
 end
