337eeb4e0cea4fcb784f7daf5bb6ab00ab8d02bf
rasna shakya <rasnashakya@gmail.com>
2017-02-27 15:04:18 +0545

    included carrier-wave-thumbnailer

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -4,6 +4,7 @@ class VideoUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
   include CarrierWave::Video
+  include CarrierWave::Video::Thumbnailer
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -35,6 +36,10 @@ class VideoUploader < CarrierWave::Uploader::Base
   # version :thumb do
   #   process resize_to_fit: [50, 50]
   # end
+
+  version :thumb do
+    process thumbnail: [{quality: 10, size: 192, strip: true, logger: Rails.logger}]    
+  end 
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
