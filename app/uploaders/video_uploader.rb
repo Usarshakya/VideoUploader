3945f20303fcfd52ea18d6c837350be9f4f2be86
rasna shakya <rasnashakya@gmail.com>
2017-02-27 10:43:16 +0545

    added whitelist extention, worked on action Index and new

--- app/uploaders/video_uploader.rb
+++ app/uploaders/video_uploader.rb
@@ -4,6 +4,7 @@ class VideoUploader < CarrierWave::Uploader::Base
   # include CarrierWave::RMagick
   # include CarrierWave::MiniMagick
 
+
   # Choose what kind of storage to use for this uploader:
   storage :file
   # storage :fog
@@ -36,9 +37,9 @@ class VideoUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(.mov .mp4 .avi .vob)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
